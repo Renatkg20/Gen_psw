92684f366d0b6354c0edf351e83410a95a1e28fc
GOGO GURU <>
2021-04-20 06:42:04 +0000

    Delete the code which is not related to the password generation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 from string import digits
 from string import punctuation
 from string import ascii_letters
-# user_input = int(input("Please inputh the length the password "))
+user_input = int(input("Please inputh the length the password "))
 
 def gen_password(len_pass):
 	symbols = ascii_letters + digits + punctuation
@@ -10,17 +10,7 @@ def gen_password(len_pass):
 	password = ''.join(secure_random.choice(symbols)for i in range(len_pass))
 	return password
 
-
-# print(gen_password(user_input))
+print(gen_password(user_input))
 
 #https://github.com/renatkg20
 
-import sys
-c = sys.stdin.readlines()
-t = (" ".join(c)).split(" ")
-
-
-n, m = int(t[0]), int(t[1])
-
-pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
-print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
